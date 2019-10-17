1e3114ed2942f0308ce9a9a370f77a24099d3174
Muhammad Khan <mkhan@wupper.mip.informatik.uni-kiel.de>
2019-10-17 14:03:48 +0200

    test stream

---
+++ tests/test_stream.py
@@ -0,0 +1,97 @@
+from queue import Queue
+from morphocut import Pipeline
+from morphocut.stream import Slice, StreamBuffer, PrintObjects, TQDM, FromIterator
+
+import pytest
+
+
+def test_TQDM():
+    # Assert that the progress bar works with stream
+    items = range(5)
+
+    with Pipeline() as pipeline:
+        result = TQDM("Description")
+
+    stream = pipeline.transform_stream(items)
+    obj = list(stream)
+
+    assert obj == [0, 1, 2, 3, 4]
+    assert result.description == 'Description'
+
+
+def test_Slice():
+    # Assert that the stream is sliced
+    items = "ABCDEFG"
+
+    with Pipeline() as pipeline:
+        result = Slice(2)
+
+    stream = pipeline.transform_stream(items)
+    obj = list(stream)
+
+    assert obj == ['A', 'B']
+
+    # Assert that the stream is sliced from the specified start and end
+    with Pipeline() as pipeline:
+        result = Slice(2, 4)
+
+    stream = pipeline.transform_stream(items)
+    obj = list(stream)
+
+    assert obj == ['C', 'D']
+
+
+def test_StreamBuffer():
+    # Assert that the stream is buffered
+    maxsize = 5
+    items = "12345"
+
+    with Pipeline() as pipeline:
+        result = StreamBuffer(maxsize)
+
+    stream = result.transform_stream(items)
+    obj = list(stream)
+
+    assert obj[0] == '1'
+    assert obj[1] == '2'
+    assert obj[2] == '3'
+    assert obj[3] == '4'
+    assert obj[4] == '5'
+
+
+class TestClass:
+
+    def __init__(self, name):
+        self.name = name
+
+
+#@pytest.mark.xfail
+def test_FromIterator():
+    # Assert that the stream is buffered
+    values = list(range(10))
+
+    with Pipeline() as pipeline:
+        value = FromIterator(values)()
+
+    stream = pipeline.transform_stream()
+
+    result = [o[value] for o in stream]
+
+    assert values == result
+
+
+@pytest.mark.xfail
+def test_PrintObjects():
+    # Assert that the stream is buffered
+    items = "12345"
+    arg = TestClass("test")
+
+    with Pipeline() as pipeline:
+        image = FromIterator([skimage.data.astronaut(), ...])()
+        PrintObjects(value)()
+
+    stream = pipeline.transform_stream(items)
+
+    list(stream)
+
+    assert next(stream) == '1'
