157b57b04d647221f8d064a87ae55cfcb96a343b
Muhammad Ammar Khan <ammark100@gmail.com>
2019-10-28 00:29:18 +0100

    test File

---
+++ tests/test_file.py
@@ -0,0 +1,20 @@
+from morphocut import Pipeline
+from morphocut.file import Find, Glob
+
+import pytest
+
+def test_Find(data_path):
+    d = data_path / "images"
+    with Pipeline() as pipeline:
+        result = Find(d, ["png"])
+
+    stream = pipeline.transform_stream()
+    pipeline.run()
+
+def test_Glob():
+    d = "images/test_image_3.png"
+    with Pipeline() as pipeline:
+        result = Glob(d, True)
+
+    stream = pipeline.transform_stream()
+    pipeline.run()
