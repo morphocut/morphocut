b80c8fcc2a99fa21b41f637ba1aa083dfd1bfdfa
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2019-11-15 13:41:38 +0100

    zooprocess: Docs and tests

--- tests/contrib/test_zooprocess.py
+++ tests/contrib/test_zooprocess.py
@@ -1,27 +1,19 @@
-from morphocut.contrib.zooprocess import CalculateZooProcessFeatures
-from tests.helpers import BinaryBlobs
+from morphocut.image import FindRegions
 from morphocut import Call, Pipeline
+from morphocut.contrib.zooprocess import CalculateZooProcessFeatures
+from tests.helpers import BinaryBlobs, NoiseImage
+import numpy as np
+from morphocut.stream import Unpack
 
 
 def test_CalculateZooProcessFeatures():
     with Pipeline() as p:
-        i = FromIterable(range(10))
+        i = Unpack(range(10))
         mask = BinaryBlobs()
-        image_name = Format("image_{}", i)
+        image = NoiseImage(mask.shape)
 
-        EcotaxaWriter(archive_fn, image, image_name, meta, image_ext=".png")
+        regionprops = FindRegions(mask, image)
 
-    result = [o.to_dict(meta=meta, image=image) for o in p.transform_stream()]
+        CalculateZooProcessFeatures(regionprops)
 
-    # Read the archive
-    with Pipeline() as p:
-        image, meta = EcotaxaReader(archive_fn)
-
-    roundtrip_result = [o.to_dict(meta=meta, image=image) for o in p.transform_stream()]
-
-    for meta_field in ("i", "foo"):
-        assert [o["meta"][meta_field] for o in result] == [
-            o["meta"][meta_field] for o in roundtrip_result
-        ]
-
-    assert_equal([o["image"] for o in result], [o["image"] for o in roundtrip_result])
+    p.run()
