0628d1c959ba3f40283d34126f25fd2cc5b899ba
Muhammad Ammar Khan <ammark100@gmail.com>
2019-11-17 04:51:02 +0100

    DrawContoursOnParent modified, Documentation and tests

--- src/morphocut/annotation.py
+++ src/morphocut/annotation.py
@@ -4,6 +4,7 @@ Operations for annotating images.
 """
 import numpy as np
 from skimage import img_as_ubyte, img_as_float
+from skimage.color import gray2rgb, rgb2gray
 from skimage.morphology import binary_dilation, disk
 import skimage.exposure
 import skimage.io
@@ -15,11 +16,29 @@ from morphocut import Node, Output, RawOrVariable, ReturnOutputs
 @ReturnOutputs
 @Output("contour")
 class DrawContours(Node):
+    """
+    Dilate the image and make boundary around the dilated image and outline pixels in the background around 
+    object boundaries.
 
-    def __init__(self, image, mask, dilate_rel=0, dilate_abs=0):
+    .. note::
+        Uses the skimage library :py:func:`skimage.morphology.binary_dilation` and :py:func:`skimage.segmentation.find_boundaries`.
+
+    Args:
+        image (np.ndarray or Variable[np.ndarray]): An image to be contoured.
+        mask (np.ndarray or Variable[np.ndarray]): Mask of an image.
+        color (tuple): Value of RGB colors as a tuple.
+        dilate_rel (int): Relative Dilation value.
+        dilate_abs (int): Absolute Dilation value.
+
+    Returns:
+        Variable[np.ndarray]: Contoured image.
+    """
+
+    def __init__(self, image, mask, color, dilate_rel=0, dilate_abs=0):
         super().__init__()
         self.image = image
         self.mask = mask
+        self.color = color
         self.dilate_rel = dilate_rel
         self.dilate_abs = dilate_abs
 
@@ -36,23 +55,41 @@ class DrawContours(Node):
         # Draw contours
         boundary_mask = skimage.segmentation.find_boundaries(dilated_mask, mode="outer")
 
-        contour_image = img_as_ubyte(image, True)
-        color = (0, 255, 0)
+        contour_image = skimage.color.gray2rgb(img_as_ubyte(image, True))
+        #color = (0, 255, 0)
 
-        contour_image[boundary_mask] = color
+        contour_image[boundary_mask] = self.color
 
         return contour_image
 
 @ReturnOutputs
 @Output("parent_contour")
 class DrawContoursOnParent(Node):
+    """
+    Dilate the image and make boundary around the dilated image and outline pixels in the background around 
+    object boundaries.
 
-    def __init__(self, image, mask, output, parent_slice, dilate_rel=0, dilate_abs=0):
+    .. note::
+        Uses the skimage library :py:func:`skimage.morphology.binary_dilation` and :py:func:`skimage.segmentation.find_boundaries`.
+
+    Args:
+        image (np.ndarray or Variable[np.ndarray]): An image to be contoured.
+        mask (np.ndarray or Variable[np.ndarray]): Mask of an image.
+        color (tuple): Value of RGB colors as a tuple.
+        dilate_rel (int): Relative Dilation value.
+        dilate_abs (int): Absolute Dilation value.
+
+    Returns:
+        Variable[np.ndarray]: Contoured image.
+    """
+
+    def __init__(self, image, mask, parent_slice, color, dilate_rel=0, dilate_abs=0):
         super().__init__()
         self.image = image
         self.mask = mask
-        self.output = output
+        #self.output = output
         self.parent_slice = parent_slice
+        self.color = color
         self.dilate_rel = dilate_rel
         self.dilate_abs = dilate_abs
 
@@ -63,26 +100,26 @@ class DrawContoursOnParent(Node):
             try:
                 parent_img = self.prepare_input(parent, ("output"))
             except KeyError:
-                parent_img = img_as_ubyte(
-                    self.image, force_copy=True)
+                parent_img = skimage.color.gray2rgb(img_as_ubyte(
+                    self.image, force_copy=True))
                 self.prepare_output(parent, parent_img)
 
-            mask = self.prepare_input(obj, ("mask"))
+            #mask = self.prepare_input(obj, ("mask"))
 
             # Calculate radius of the dilation
-            area = np.sum(mask)
+            area = np.sum(self.mask)
             radius = self.dilate_rel * np.sqrt(area) + self.dilate_abs
 
             # Dilate mask
             dilated_mask = binary_dilation(
-                mask,
+                self.mask,
                 disk(radius))
 
             # Draw contours
             boundary_mask = skimage.segmentation.find_boundaries(dilated_mask, mode="outer")
 
-            color = (0, 255, 0)
+            #color = (0, 255, 0)
 
-            parent_img[self.parent_slice][boundary_mask] = color
+            parent_img[self.parent_slice][boundary_mask] = self.color
 
-            yield self.prepare_output(obj)
+            yield self.prepare_output(parent_img)
--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -1,21 +1,30 @@
-import numpy as np
 import pytest
 import skimage.io
 
 from morphocut import Pipeline
-from morphocut.file import Glob
 from morphocut.annotation import (
     DrawContours,
     DrawContoursOnParent,
 )
-from morphocut.image import ThresholdConst
+from morphocut.image import ThresholdConst, FindRegions, ExtractROI
 
 
-def test_ThresholdConst():
+def test_DrawContours():
     image = skimage.data.camera()
     with Pipeline() as pipeline:
         mask = ThresholdConst(image, 255)
-        result = DrawContours(image, mask)
+        result = DrawContours(image, mask, (0, 255, 0))
+
+    stream = pipeline.transform_stream()
+    pipeline.run()
+
+def test_DrawContoursOnParent():
+    image = skimage.data.camera()
+    with Pipeline() as pipeline:
+        mask = ThresholdConst(image, 255)
+        regions = FindRegions(mask, image, 0, 100, padding=10)
+        #extracted_images = ExtractROI(image, mask, regions)
+        result = DrawContoursOnParent(image, mask, regions, (0, 255, 0))
 
     stream = pipeline.transform_stream()
     pipeline.run()
