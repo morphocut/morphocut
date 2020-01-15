520d639d95951e56946a22f0c6a5e39d627c4d7b
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2020-01-15 09:34:57 +0100

    Profile: Measure the lead time of object in the pipeline
    - core.Variable: Allow all StreamTransformers as parent
    - tests.helpers: Adopt Sleep from test_parallel

---
+++ docs/profiling.rst
@@ -0,0 +1,9 @@
+Profiling
+=========
+
+:py:class:`~morphocut.profile.Profile` enables the measurement of the time
+an object spends inside certain parts of the pipeline.
+
+.. autoclass:: morphocut.profile.Profile
+    :members:
+    :exclude-members: transform_stream
---
+++ tests/test_profile.py
@@ -0,0 +1,26 @@
+import pytest
+from timer_cm import Timer
+
+from morphocut import Node, Pipeline
+from morphocut.profile import Profile
+from morphocut.stream import Unpack
+from tests.helpers import Sleep
+
+N = 1000
+DURATION = 0.001
+
+
+def test_Profile():
+
+    with Pipeline() as pipeline:
+        Unpack(range(N))
+        with Profile("Sleep", summary=False) as profile_sleep:
+            Sleep()
+
+    objects = list(pipeline.transform_stream())
+
+    assert len(objects) == N
+
+    overhead = profile_sleep._average - DURATION
+
+    print(f"Overhead {overhead:g}s")
