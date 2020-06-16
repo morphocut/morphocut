5b1cc02bc27bcf19df78087ac8cd4dd91c6d505f
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2020-06-16 21:10:17 +0200

    Coverage stat.py.

--- tests/test_stat.py
+++ tests/test_stat.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from morphocut import Pipeline
-from morphocut.stat import RunningMedian
+from morphocut.stat import RunningMedian, ExponentialSmoothing
 from morphocut.stream import Unpack
 
 
@@ -17,5 +17,13 @@ def test_RunningMedian_numpy():
     with Pipeline() as p:
         value = Unpack(np.arange(20)[:, np.newaxis] * np.ones((10)))
         running_median = RunningMedian(value, n_init=2)
+
+    p.run()
+
+
+def test_ExponentialSmoothing():
+    with Pipeline() as p:
+        value = Unpack(range(20))
+        running_median = ExponentialSmoothing(value, 0.5)
 
     p.run()
