9433c1cb19c5870653810cf55e26167fb971712e
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2020-06-16 20:35:08 +0200

    Coverage file

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,15 +1,29 @@
-from morphocut import Pipeline
-from morphocut.file import Find, Glob
+import re
 
 import pytest
 
-def test_Find(data_path):
+from morphocut import Pipeline
+from morphocut.file import Find, Glob
+
+
+@pytest.mark.parametrize("sort", [True, False])
+@pytest.mark.parametrize("verbose", [True, False])
+def test_Find(data_path, sort, verbose, capsys):
     d = data_path / "images"
     with Pipeline() as pipeline:
-        result = Find(d, [".png"])
+        filename = Find(d, [".png"], sort, verbose)
 
     stream = pipeline.transform_stream()
-    pipeline.run()
+
+    filenames = [o[filename] for o in stream]
+
+    if sort:
+        assert filenames == sorted(filenames)
+
+    if verbose:
+        out = capsys.readouterr().out
+        assert re.search(r"^Found \d+ files in .+\.$", out)
+
 
 def test_Glob(data_path):
     d = data_path / "images/*.png"
