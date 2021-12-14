d6dc61866744d6662f505a854262b63415aa2957
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2021-12-14 11:12:49 +0000

    feat(hdf5.HDF5Writer): Write arrays to a HDF5 file

---
+++ tests/test_hdf5.py
@@ -0,0 +1,27 @@
+import itertools
+
+import h5py
+import pytest
+
+from morphocut.batch import BatchPipeline
+from morphocut.core import Call, Pipeline
+from morphocut.hdf5 import HDF5Writer
+from morphocut.stream import Unpack
+from morphocut.stream_estimator import RemainingHint
+import itertools
+
+
+def test_HDF5Writer_extend(tmp_path):
+    h5_fn = tmp_path / "test.h5"
+    values = [range(10) for _ in range(100)]
+    with Pipeline() as pipeline:
+        arr = Unpack(values)
+        HDF5Writer(h5_fn, {"arr": arr}, dataset_mode="extend")
+
+    pipeline.run()
+
+    with h5py.File(h5_fn, "r") as h5f:
+        h5_values = list(h5f["arr"][:])
+
+    values_expected = list(itertools.chain(*values))
+    assert h5_values == values_expected
