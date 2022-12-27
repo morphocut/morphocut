80905b13eae73d28f24bfce8340c9b0a8770932b
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2022-12-27 18:04:09 +0100

    Test groupby

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 from morphocut.batch import BatchPipeline
 from morphocut.core import Call, Pipeline
 from morphocut.stream import Unpack
@@ -46,3 +47,23 @@ def test_BatchPipeline(seq_len):
     # print("remaining2", [r[remaining2] for r in result])
 
     assert [r[remaining0] for r in result] == [r[remaining2] for r in result]
+
+def assert_(cond):
+    assert cond
+
+@pytest.mark.parametrize(
+    "seq_len",
+    [5, 10, 100, 111],
+)
+def test_BatchPipeline_groupby(seq_len):
+    batch_size = 10
+    values = list(range(seq_len))
+    with Pipeline() as pipeline:
+        a = Unpack(values)
+        b = Unpack(values)
+        with BatchPipeline(batch_size, groupby=a):
+            # a is scalar, b is a sequence
+            Call(lambda a: assert_(isinstance(a, int)), a)
+            Call(lambda b: assert_(isinstance(b, Sequence)), b)
+
+    result = list(pipeline.transform_stream())
