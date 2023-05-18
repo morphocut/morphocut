d11576e087802c45c9cb0eccb4b72ca4b7c4d575
Saeideh Sadeghpour <s.sadeghpour97@gmail.com>
2023-05-18 13:29:20 -0400

    feat: add test for pipeline

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,4 +1,4 @@
-from morphocut.pipelines import MergeNodesPipeline, DataParallelPipeline
+from morphocut.pipelines import MergeNodesPipeline, DataParallelPipeline, AggregateErrorsPipeline
 from morphocut.core import Call, Pipeline
 from morphocut.stream import Unpack
 import pytest
@@ -27,3 +27,28 @@ def test_DataParallelPipeline():
 
     with pytest.raises(ZeroDivisionError):
         p3.run()
+
+
+def test_MergeNodesPipeline():
+    with Pipeline() as p:
+        a = Unpack(range(100))
+        with MergeNodesPipeline() as m:
+            b = Call(lambda x: x + 1, a)
+            c = Call(lambda x: x * 2, a)
+
+    expected_b = [obj[b] for obj in p.transform_stream()]
+    expected_c = [obj[c] for obj in p.transform_stream()]
+
+    assert expected_b == list(range(1, 101))
+    assert expected_c == list(range(0, 200, 2))
+
+
+def test_AggregateErrorsPipeline():
+    with Pipeline() as p:
+        a = Unpack(range(100))
+        with AggregateErrorsPipeline() as m:
+            b = Call(lambda x: x + 1, a)
+            c = Call(lambda x: x / 0 if x == 50 else x, a)
+
+    with pytest.raises(ZeroDivisionError):
+        p.run()
