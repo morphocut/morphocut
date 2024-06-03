3511fc8b68edf511c8072c39211444784c1228b0
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2024-06-03 16:56:49 +0200

    Drop Python 3.9 support.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "scipy",
         "deprecated",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     extras_require={
         "tests": [
             # Pytest
@@ -52,7 +52,6 @@ setup(
     entry_points={},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
